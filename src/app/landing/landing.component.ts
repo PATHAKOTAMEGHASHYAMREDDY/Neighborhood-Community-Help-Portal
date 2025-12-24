c32e9023243212cabbb1fc1f1091c746b00103b0
shyam <meghashyamreddy014@gmail.com>
2025-12-24 11:25:30 +0530

    Lottie has been added

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import lottie, { AnimationItem } from 'lottie-web';
 
 @Component({
   selector: 'app-landing',
@@ -8,8 +9,35 @@ import { Router } from '@angular/router';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('lottieContainer') lottieContainer!: ElementRef;
+  private animation: AnimationItem | null = null;
+
   constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.loadAnimation();
+    }, 0);
+  }
+
+  ngOnDestroy() {
+    if (this.animation) {
+      this.animation.destroy();
+    }
+  }
+
+  loadAnimation() {
+    if (this.lottieContainer && this.lottieContainer.nativeElement) {
+      this.animation = lottie.loadAnimation({
+        container: this.lottieContainer.nativeElement,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '/Community_services.json'
+      });
+    }
+  }
 
   navigateToAuth() {
     this.router.navigate(['/register']);
